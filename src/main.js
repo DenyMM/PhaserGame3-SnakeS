f39ab01983c0c0f8f3cbe5caca7b397799787e54
DenyMM <deny.lolipop@gmail.com>
2019-02-26 00:42:02 -0600

    Se agrego estructura

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const config = {
+  tittle:'SnakeS',
+  width: 320,
+  height: 180,
+  type: Phaser.AUTO,
+  parent: 'container',
+  backgroundColor: 'f9ca24',
+  pixelArt: true,
+};
+
+new Phaser.Game(config);
